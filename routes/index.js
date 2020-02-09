8048c0b99d705ac290030e18560c44cfead1016a
pledovski <human.pled@gmail.com>
2020-02-09 15:57:57 +0200

    basic routing

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.route("/").get((req, res, next) => {
+  res.render("index", {
+    pageTitle: "Welcome to Pledov dev server"
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,6 +8,12 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// Routes
+const indexView = require("./routes/index");
+
 // Init middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -15,8 +21,11 @@ app.use(bodyParser.json());
 //Static files
 app.use(express.static(path.join(__dirname, "public")));
 
+//Mount view routes
+app.use("/", indexView);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "127.0.0.1", () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
