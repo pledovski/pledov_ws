bacaff9730c0adfcb349a908a8269cea344b87c6
pledovski <human.pled@gmail.com>
2020-12-22 15:44:29 +0200

    message logger

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Content = require("../models/content");
 
 router.route("/").get(async (req, res, next) => {
-  const content = await Content.findOne({}).sort({ createdAt: -1 });
+  const content = await Content.findOne({}).sort({ createdAt: 1 });
   res.render("index", content);
 });
 
